d83f94aa7a3d8433e404bc3f89a47be7840c7ab3
rohit-kumar22 <83944002+rohit-kumar22@users.noreply.github.com>
2023-05-10 00:14:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Dashboard from "./components/Dashboard";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.DEV ? "/" : "/react-vite-gh-pages/"}>
         <Routes>
           <Route exact path="/" element={<SignUp />} />
           <Route exact path="/dashboard" element={<Dashboard />} />
